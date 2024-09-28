ba3c09b5b0673750b61207b518bf413b1f2c9162
Nika <nikapanchenko94@gmail.com>
2024-09-28 12:38:03 +0300

    corrected mistakes

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       data: contacts,
       message: 'Successfully found contacts!',
     });
@@ -37,6 +38,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contact with id ${contactId}!',
       data: contact,
     });
